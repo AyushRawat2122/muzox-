7f8f75ad29d45d7e36fa3b1f6c3b371857e1f490
AyushRawat2122 <ayushprovalorant@gmail.com>
2025-02-22 00:25:08 +0530

    files renamed

--- backend/src/router/user.routes.js
+++ backend/src/router/user.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import signup from "../controller/user.controller.js";
+import signup from "../controllers/user.controller.js";
 const userRouter=express.Router();
 userRouter.post('/signup',signup);
 export default userRouter;
