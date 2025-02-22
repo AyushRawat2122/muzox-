325c11e96bb3891f7a84206356a3e0597fb4d6de
roshan-jha-23 <jha062305@gmail.com>
2025-02-22 23:27:33 +0530

    controller of register and verify email

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,11 +3,23 @@ import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import bcryptjs from "bcryptjs";
 import { sendEmail } from "../utils/mailer.js";
-import ApiResponse from "../utils/ApiResponse.js"; 
-
-
-const signup = asyncHandler(async (req, res) => {
-  const { username, email, password, profilePicture, coverPicture } = req.body;
+import ApiResponse from "../utils/ApiResponse.js";
+import { uploadOnCloudinary,deleteOnCloudinary } from "../utils/cloudinary.js";
+export const signup = asyncHandler(async (req, res) => {
+ /* //get user details from client
+  //validate the details
+  //now check the database using email or password
+  //now check if image if it has came or not
+  //if image is available then upload on SDK
+  //hash the password
+  //create otp for verification purpose
+  //set expiry time for verification now
+  //create the user in db [create the user]
+  //once the user is created send email consisting of otp
+  //if any error has came in response send that response
+  //otherwise send the success response*/
+  console.log("aya");
+  const { username, email, password } = req.body;
   if (!username || !email || !password) {
     throw new ApiError(400, "All fields are required");
   }
@@ -17,35 +29,67 @@ const signup = asyncHandler(async (req, res) => {
   if (alreadyAUser) {
     throw new ApiError(400, "User already exists");
   }
+  console.log("hi");
+  const profilePicLocal = req.files?.profilePic[0]?.path;
+  console.log(profilePicLocal);
 
+  const profilePic = await uploadOnCloudinary(profilePicLocal);
   const salt = await bcryptjs.genSalt(10);
   const hashedPassword = await bcryptjs.hash(password, salt);
-
   const otp = Math.floor(100000 + Math.random() * 900000).toString();
   const expiryDate = new Date(Date.now() + 3600000);
-
-
   const user = await User.create({
     username: username.toLowerCase(),
     email: email.toLowerCase(),
     password: hashedPassword,
-    profilePicture: profilePicture || "", 
-    coverPicture: coverPicture || "",       
+    profilePic: profilePic.url || "",
     verifyToken: otp,
     verifyTokenExpiry: expiryDate,
   });
-
-  sendEmail({
-    email,
-    otp,
-    username,
-    option: "VERIFY",
+  await sendEmail({
+    email: email,
+    otp: otp,
+    name: username,
+    type: "VERIFY",
     userId: user._id,
   });
-
   return res
     .status(201)
     .json(new ApiResponse(200, user, "User registered successfully"));
 });
+export const verifyUser=asyncHandler(async(req,res)=>{
+   const { userId } = req.params;
+   const { otp } = req.body;
 
-export default signup;
+  if (!otp) {
+    // If no OTP  delete the user after 3 minutes
+    setTimeout(async () => {
+      const user=await User.findById(userId);
+      await User.findByIdAndDelete(userId);
+      const urlOfProfilePic=user.profilePic;
+      await deleteOnCloudinary(urlOfProfilePic)
+      console.log(`Unverified user ${userId} deleted both from db and cloudinary`);
+    }, 180000); 
+    throw new ApiError(400, "OTP required for verification");
+  }
+
+  // Find the user
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+
+  // Match OTP
+  if (user.verifyToken !== otp || user.verifyTokenExpiry < new Date()) {
+    throw new ApiError(400, "Invalid or expired OTP");
+  }
+
+  // finally save 
+  user.isVerified = true;
+  user.verifyToken = undefined;
+  user.verifyTokenExpiry = undefined;
+  await user.save();
+
+  return res.status(200).json(new ApiResponse(200, user, "User verified successfully"));
+})
+export default {signup,verifyUser};
--- backend/src/router/user.routes.js
+++ backend/src/router/user.routes.js
@@ -1,5 +1,11 @@
 import express from "express";
-import signup from "../controllers/user.controller.js";
-const userRouter=express.Router();
-userRouter.post('/signup',signup);
+import uploader from "../middlewares/multer.middleware.js";
+import {signup,verifyUser} from "../controllers/user.controller.js";
+const userRouter = express.Router();
+userRouter.post(
+  "/signup",
+  uploader.fields([{ name: "profilePic", maxCount: 1 }]),
+  signup
+);
+userRouter.post("/verifyUser/:userId",verifyUser);
 export default userRouter;
