932ee367bde4a55fbf86190cdca7d419a09f1439
AyushRawat2122 <ayushprovalorant@gmail.com>
2025-04-09 23:29:46 +0530

    buffer test1

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage();
 const uploader = multer({
     storage: storage,
     limits: {
-      fileSize: 10 * 1024 * 1024, // 10MB limit
+      fileSize: 100 * 1024 * 1024, // 100MB limit
     },
   });
 
