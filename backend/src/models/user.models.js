54a07dede694b5ebcb2a67e700e2c6e279a9c11b
AyushRawat2122 <ayushprovalorant@gmail.com>
2025-04-24 10:37:29 +0530

    model fix - username unique constraint withdrawn

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -7,7 +7,6 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      unique: true,
       required: [true, "Username is required"],
       lowercase: true,
       trim: true,
